3cc4558a0ccf5e3f0e8d9e04275bb6a32c347045
Utkarsh Aggarwal <utkarshaggarwalcse@gmail.com>
2024-05-14 22:49:58 +0530

    # unpack ethernet frame

--- sniffer_demo.py
+++ sniffer_demo.py
@@ -8,3 +8,6 @@ def ethernet_frame(data):
     dest_mac, src_mac , proto = struct.unpack('! 6s 6s H', data[:14])
     return get_mac_addr(dest_mac), get_mac_addr(src_mac), socket.htons(proto) , data[14:]
 
+# return properly formatted MAC address
+def get_mac_addr(bytes_addr):
+    bytes_str = map('{:02x}'.format, bytes_addr)
