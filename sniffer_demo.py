92022dc6e5bdbf9291cb87f862668278622d9626
Utkarsh Aggarwal <utkarshaggarwalcse@gmail.com>
2024-05-14 22:45:05 +0530

    # unpack ethernet frame

--- sniffer_demo.py
+++ sniffer_demo.py
@@ -1,3 +1,10 @@
 import socket
 import struct
 import textwrap
+
+# unpack ethernet frame
+
+def ethernet_frame(data):
+    dest_mac, src_mac , proto = struct.unpack('! 6s 6s H', data[:14])
+    return get_mac_addr(dest_mac), get_mac_addr(src_mac), socket.htons(proto) , data[14:]
+
