29f06cf0dac3719e8d3ef40fd84f0545cfafb3f8
Utkarsh Aggarwal <utkarshaggarwalcse@gmail.com>
2024-05-14 22:50:10 +0530

    # return properly formatted MAC address (ie AA:BB:CC:DD:EE:FF)

--- sniffer_demo.py
+++ sniffer_demo.py
@@ -8,6 +8,9 @@ def ethernet_frame(data):
     dest_mac, src_mac , proto = struct.unpack('! 6s 6s H', data[:14])
     return get_mac_addr(dest_mac), get_mac_addr(src_mac), socket.htons(proto) , data[14:]
 
-# return properly formatted MAC address
+# return properly formatted MAC address (ie AA:BB:CC:DD:EE:FF)
 def get_mac_addr(bytes_addr):
     bytes_str = map('{:02x}'.format, bytes_addr)
+    mac_addr = ':'.join(bytes_str).upper()
+    
+
